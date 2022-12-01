1f4163c0c9418159d28021e63c5b66a0ccbcc1f4
alin090402 <alin090402@yahoo.ro>
2022-12-02 01:06:57 +0200

    Created getCharacterDto

--- WorkForever/Dtos/Character/GetCharacterDto.cs
+++ WorkForever/Dtos/Character/GetCharacterDto.cs
@@ -1,9 +1,7 @@
-﻿namespace WorkForever.Models;
+﻿namespace WorkForever.Dtos.Character;
 
-public class Character
+public class GetCharacterDto
 {
-    public int Id { get; set; }
     public string Username { get; set; } = string.Empty;
     public double WorkExperience { get; set; }
-    
 }
